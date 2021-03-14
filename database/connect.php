1a1698eed87b9eb9015dd92af3bbb4e393c73850
sines210 <sines210@gmail.com>
2021-03-14 22:48:14 +0100

    commit

--- database/connect.php
+++ database/connect.php
@@ -1,11 +1,15 @@
 <?php
 
-
+$userName = "bd2c7dbc271629";
+$pass = "1f2aea8b";
+$host = "localhost";
+$dbName = "chat_simplon";
 
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbName", $userName, $pass);
+    $db = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_5a36dd97718ac9d", $userName, $pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+
